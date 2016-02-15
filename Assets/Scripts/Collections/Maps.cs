aeef5592a61bc750cdb78ea027a9082b9a15b3d0
Darren Eidson <dse2pd@mail.missouri.edu>
2016-02-14 19:35:44 -0600

    Map now loads from generated json on server.

---
+++ Assets/Scripts/Collections/Maps.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Maps : MonoBehaviour {
+
+	public string jsonMapLocation;
+	public GameObject mapModel;
+
+	public List<GameObject> maps;
+
+	IEnumerator Start() {
+		if (jsonMapLocation != "") {
+			// Load JSON data from a URL
+			WWW www = new WWW(jsonMapLocation);
+
+			// Load the data and yield (wait) till it's ready before we continue executing the rest of this method.
+			yield return www;
+			if (www.error == null) {
+				processMaps(www.text);
+			} else {
+				Debug.Log("ERROR: " + www.error);
+			}
+		}
+	}
+
+	private void processMaps (string jsonString) {
+		JsonData jsonMaps = JsonMapper.ToObject (jsonString);
+		maps = new List<GameObject> ();
+		Map map;
+		Map mapScript;
+
+		for (int i = 0; i < jsonMaps ["maps"].Count; i++) {
+			GameObject newMap = Instantiate (mapModel, Vector3.zero, Quaternion.identity) as GameObject;
+			newMap.transform.parent = gameObject.transform;
+
+			mapScript = newMap.GetComponent<Map> ();
+			mapScript.createMap (jsonMaps ["maps"] [i], (i == 0));
+
+			maps.Add (newMap);
+		}
+	}
+}
---
+++ Assets/Scripts/Models/Map.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map : MonoBehaviour {
+
+	public static Map S;
+
+	public List<GameObject> tileTypes;
+
+	private int mapWidth;
+	private List<int> tileTypeIds;
+
+	public void Awake () {
+		S = this;
+
+		tileTypeIds = new List<int> ();
+	}
+
+	public void createMap (JsonData jsonData, bool boolBuildMap) {
+		Debug.Log (jsonData [1].ToString());
+		mapWidth = int.Parse (jsonData ["width"].ToString ());
+
+		for (int i = 0; i < jsonData ["tiles"].Count; i++) {
+			tileTypeIds.Add (int.Parse (jsonData ["tiles"] [i] ["type"].ToString ()));
+		}
+
+		if (boolBuildMap) {
+			StartCoroutine (waitToBuildMap ());
+		}
+	}
+
+	public void buildMap () {
+		int row = 0;
+		int j = 0;
+
+		for (int i = 0; i < tileTypeIds.Count; i++) {
+			Vector3 pos = new Vector3 (j, 0, row);
+			GameObject tile = Instantiate (tileTypes [tileTypeIds [i] - 1], pos, Quaternion.identity) as GameObject;
+			tile.transform.parent = gameObject.transform;
+
+			if (j >= (mapWidth - 1)) {
+				j = 0;
+				row++;
+			} else {
+				j++;
+			}
+		}
+	}
+
+	IEnumerator waitToBuildMap () {
+		yield return new WaitForSeconds (5);
+		buildMap ();
+	}
+}
