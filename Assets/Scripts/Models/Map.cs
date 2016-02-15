2b2ec959884f15184e8ce86291051305113b39e4
Darren Eidson <dse2pd@mail.missouri.edu>
2016-02-14 21:53:44 -0600

    fixed issue with map and map tiles both being flipped vertically

--- Assets/Scripts/Models/Map.cs
+++ Assets/Scripts/Models/Map.cs
@@ -10,11 +10,14 @@ public class Map : MonoBehaviour {
 	public List<GameObject> tileTypes;
 
 	private int mapWidth;
+	private List<int> tileRotations;
 	private List<int> tileTypeIds;
 
 	public void Awake () {
+
 		S = this;
 
+		tileRotations = new List<int> ();
 		tileTypeIds = new List<int> ();
 	}
 
@@ -23,6 +26,7 @@ public class Map : MonoBehaviour {
 		mapWidth = int.Parse (jsonData ["width"].ToString ());
 
 		for (int i = 0; i < jsonData ["tiles"].Count; i++) {
+			tileRotations.Add (int.Parse (jsonData ["tiles"] [i] ["rotateY"].ToString ()));
 			tileTypeIds.Add (int.Parse (jsonData ["tiles"] [i] ["type"].ToString ()));
 		}
 
@@ -35,9 +39,11 @@ public class Map : MonoBehaviour {
 		int row = 0;
 		int j = 0;
 
-		for (int i = 0; i < tileTypeIds.Count; i++) {
-			Vector3 pos = new Vector3 (j, 0, row);
+		for (int i = 0; i < Mathf.Min(tileRotations.Count, tileTypeIds.Count); i++) {
+			Vector3 pos = new Vector3 (j, 0, (row * -1));
 			GameObject tile = Instantiate (tileTypes [tileTypeIds [i] - 1], pos, Quaternion.identity) as GameObject;
+			tile.name = string.Format ("[{0},{1}] - {2}", j, row, tileTypes [tileTypeIds [i] - 1].name);
+			tile.transform.Rotate (0, (180 + tileRotations [i]), 0);
 			tile.transform.parent = gameObject.transform;
 
 			if (j >= (mapWidth - 1)) {
